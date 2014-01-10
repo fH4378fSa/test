c56d35fa288638925e951cb5c9658510282dc48a
TUeno <TUeno@TUeno-PC>
2014-01-10 16:31:47 +0900

    初回コミット

---
+++ newPHPClass.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of newPHPClass
+ *
+ * @author TUeno
+ */
+class newPHPClass {
+	//put your code here
+}
+
+?>
